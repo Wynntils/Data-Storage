d0bb2ca08190dcd018d59c97b0fbb2e94152484a
Magnus Ihse Bursie <mag@icus.se>
2021-12-12 00:49:41 +0000

    Improving Ragni and Newcomers area

--- scripts/update-journeymap.sh
+++ scripts/update-journeymap.sh
@@ -44,6 +44,7 @@ elif [[ $COMMAND == "update-raw-map" ]]; then
   cp -a $WYNNDATA_DIR/journeymap-data/DIM0/day/[0-5],-[1-9].png $WYNNDATA_DIR/journeymap-data/DIM0/day/-[1-5],-[1-9].png $WYNNDATA_DIR/journeymap-data/DIM0/day/[0-5],-1[0-3].png $WYNNDATA_DIR/journeymap-data/DIM0/day/-[1-5],-1[0-3].png $WYNNDATA_DIR/journeymap-data/DIM0/day/0,0.png $TMPDIR/DIM0/day/
   echo "Using java:"
   java -version
+  # for syntax regarding journeymaptools-0.3.jar, see https://journeymap.info/JourneyMapTools
   java -jar $WYNNDATA_DIR/bin/journeymaptools-0.3.jar MapSaver $TMPDIR $WYNNDATA_DIR/rawmap/map-raw.png 512 512 -1 0 false day
   rm -rf $TMPDIR
   echo "Rawmap updated. Please go to $WYNNDATA_DIR and commit and push your changes"
