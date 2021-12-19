a8a8aa25d607fe83ab0828405facbb8463cf3f08
Magnus Ihse Bursie <mag@icus.se>
2021-12-20 00:45:01 +0100

    Update script tooextract locations from wynncraft

--- scripts/update-wynn-locations.sh
+++ scripts/update-wynn-locations.sh
@@ -12,10 +12,18 @@ jq '.' < wynn-locations.json > wynn-locations-nice.json
 jq -r '.locations[].icon' < wynn-locations-nice.json | sort -u > all-keys.txt
 
 # These can't be detected by Wynntils label detector
-jq --arg ids "Content_UltimateDiscovery.png Content_Cave.png Content_GrindSpot.png Special_FastTravel.png Special_LightRealm.png Special_RootsOfCorruption.png" '($ids / " ") as $ids | .locations[] | select (.icon | IN($ids[])) | del(.icon)' wynn-locations-nice.json > needed.json
+jq --arg ids "Content_UltimateDiscovery.png Content_Cave.png Content_GrindSpot.png Special_FastTravel.png Special_LightRealm.png Special_RootsOfCorruption.png" '($ids / " ") as $ids | .locations[] | select (.icon | IN($ids[])) | del(.icon)' wynn-locations-nice.json | jq -s '.' > needed.json
 
 # These needs to be corroborated against the Wynntils label detector
-jq --arg ids "Content_Dungeon.png Content_Raid.png Content_Miniquest.png Content_BossAltar.png" '($ids / " ") as $ids | .locations[] | select (.icon | IN($ids[])) | del(.icon)' wynn-locations-nice.json > check.json
+jq --arg ids "Content_Dungeon.png Content_Raid.png Content_Miniquest.png Content_BossAltar.png" '($ids / " ") as $ids | .locations[] | select (.icon | IN($ids[])) | del(.icon)' wynn-locations-nice.json | jq -s '.' > check.json
 
 # These probably point to Quest starter NPC characters. Needs to be checked.
-jq '.locations[] | select (.icon | contains( "Content_Quest.png")) | del(.icon)' wynn-locations-nice.json > quests.json
+jq '.locations[] | select (.icon | contains( "Content_Quest.png")) | del(.icon)' wynn-locations-nice.json | jq -s '.' > quests.json
+
+json_to_csv() {
+    jq -r '(map(keys) | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $cols, $rows[] | @csv' $1 | tail -n +2 | sort -u > $2
+}
+
+json_to_csv needed.json needed.csv
+json_to_csv check.json check.csv
+json_to_csv quests.json quests.csv
