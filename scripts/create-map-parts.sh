9c5c88245808cc99898d5384708188b418d9887f
Magnus Ihse Bursie <mag@icus.se>
2022-12-09 22:40:27 +0100

    Add mask and mapping instructions for Wynnter fair area

--- scripts/create-map-parts.sh
+++ scripts/create-map-parts.sh
@@ -116,6 +116,7 @@ do_map "Realm of Light" "realm-of-light" -3 -2 -13 -12
 do_map "Seaskipper" "seaskipper" 31 32 30 30
 do_map "Sunset Valley" "sunset-valley" -4 -3 19 19
 do_map "The Void" "void" 26 27 -10 -7
+do_map "Wynnter Fair" "wynnter-fair" -3 -2 31 32
 
 # Remove the trailing comma
 $HEAD -n -1 $JSON_METADATA_FILE > $JSON_METADATA_FILE.tmp
