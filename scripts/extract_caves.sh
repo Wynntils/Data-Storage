002cd275f606a6e1f18849db51229b334e252ccf
Kristof Kovacs <49001742+kristofbolyai@users.noreply.github.com>
2023-07-23 20:27:19 +0200

    fix: fix script

--- scripts/extract_caves.sh
+++ scripts/extract_caves.sh
@@ -5,4 +5,4 @@ base_dir="$(cd $(dirname "$0")/.. 2>/dev/null && pwd)"
 mkdir -p $base_dir/content/out
 touch $base_dir/content/out/cave.json
 rm $base_dir/content/out/cave.json
-cat $base_dir/content/content_book_dump.json | jq '.cave' | jq '.[].requirements=.[].requirements.level' > $base_dir/content/out/cave.json
+cat $base_dir/content/content_book_dump.json | jq '.cave' | jq '.[].requirements |= .level' > $base_dir/content/out/cave.json
