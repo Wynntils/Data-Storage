d8ce4744a7e4e5adf2c1e71f1f530839febc5176
DonkeyBlaster <57310593+DonkeyBlaster@users.noreply.github.com>
2023-12-25 17:36:48 -0800

    new scripts, new map files

--- scripts/update-journeymap.sh
+++ scripts/update-journeymap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This script helps the user update the map for Wynntils, using journeymap.
-# Please install journeymap 5.7, found in the bin directory.
+# Please install journeymap 5.9, found in the bin directory.
 #
 # Made by magicus (https://github.com/magicus)
 #
@@ -34,23 +34,23 @@ elif [[ $COMMAND == "install-in-journeymap" ]]; then
   cp -f $WYNNDATA_DIR/journeymap-data/DIM0/day/* journeymap/data/mp/$WYNNCRAFT_WORLD_NAME/DIM0/day
   echo "Your journeymap installation now has the latest map data"
 elif [[ $COMMAND == "install-wynntils-config" ]]; then
-  if [[ ! -d journeymap/config/5.7 ]]; then
-      echo "Cannot find journeymap 5.7 config directory"
-      echo "Please verify that you are in the correct directory and that you have journeymap 5.7 installed"
+  if [[ ! -d journeymap/config/5.9 ]]; then
+      echo "Cannot find journeymap 5.9 config directory"
+      echo "Please verify that you are in the correct directory and that you have journeymap 5.9 installed"
       exit 1
   fi
-  cp -a journeymap/config/5.7/journeymap.core.config journeymap/config/5.7/journeymap.core.config.orig
+  cp -a journeymap/config/5.9/journeymap.core.config journeymap/config/5.9/journeymap.core.config.orig
   cp -a journeymap/colorpalette.json journeymap/colorpalette.json.orig
-  cp $WYNNDATA_DIR/config/journeymap.core.config journeymap/config/5.7/journeymap.core.config
+  cp $WYNNDATA_DIR/config/journeymap.core.config journeymap/config/5.9/journeymap.core.config
   cp $WYNNDATA_DIR/config/colorpalette.json journeymap/colorpalette.json
   echo "Replaced journeymap.core.config and colorpalette.json (backups saved as .orig)"
 elif [[ $COMMAND == "restore-orig-config" ]]; then
-  if [[ ! -d journeymap/config/5.7 ]]; then
-      echo "Cannot find journeymap 5.7 config directory"
-      echo "Please verify that you are in the correct directory and that you have journeymap 5.7 installed"
+  if [[ ! -d journeymap/config/5.9 ]]; then
+      echo "Cannot find journeymap 5.9 config directory"
+      echo "Please verify that you are in the correct directory and that you have journeymap 5.9 installed"
       exit 1
   fi
-  cp -a journeymap/config/5.7/journeymap.core.config.orig journeymap/config/5.7/journeymap.core.config
+  cp -a journeymap/config/5.9/journeymap.core.config.orig journeymap/config/5.9/journeymap.core.config
   cp -a journeymap/colorpalette.json.orig journeymap/colorpalette.json
   echo "Restored journeymap.core.config and colorpalette.json from backups"
 else
